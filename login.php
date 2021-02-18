41435d3c62bb91060b70f3e7e9e54388414c13b6
Matthew Adlam <mattadlam3@gmail.com>
2021-02-18 13:20:02 +1300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Review</title>
+    <link rel="stylesheet" href="style.css" type="text/css" charset="utf-8">
+</head>
+<body>
+    <header class = "header-text"> Book Review </header>
+
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="signup.html">Sign Up</a>
+      <a href="login.html">Log In</a>
+    </nav>
+</body>
+</html>
